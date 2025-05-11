db4dba48d33db09e39ca97be7d6b554345d682d5
choyeongju <jjudy1301@naver.com>
2025-05-11 23:40:15 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/main/java/org/sopt/hyundai/HyundaiApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.hyundai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundaiApplication.class, args);
+	}
+
+}
